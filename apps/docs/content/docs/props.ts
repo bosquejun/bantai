8bd491d1928073149ff58ccac279eec5ace13145
Jun Bosque <bosque.junpaul@gmail.com>
2026-02-04 02:36:19 +0800

    remove props.ts

--- apps/docs/content/docs/props.ts
+++
@@ -1,6 +0,0 @@
-import { ContextDefinition } from '@bantai-dev/core';
-import { z } from 'zod';
-
-export type DefineRuleParameters<T extends z.ZodRawShape> = {
-    context: ContextDefinition<T, Record<string, unknown>>;
-}
